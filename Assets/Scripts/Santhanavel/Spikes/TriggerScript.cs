bd352b9c0f6cc0e506474a07c678518a767fd788
santhoshleagueX <santhoshnagarajan321@gmail.com>
2025-12-02 22:56:59 +0530

    Modified TriggerScript

--- Assets/Scripts/Santhanavel/Spikes/TriggerScript.cs
+++ Assets/Scripts/Santhanavel/Spikes/TriggerScript.cs
@@ -1,15 +1,13 @@
 using UnityEngine;
 using UnityEngine.Events;
 
-public class MoveableSpikeTrigger_RS : MonoBehaviour
+public class TriggerScript : MonoBehaviour
 {
-    [SerializeField] private MoveableSpike_RS spike;
     [SerializeField] private UnityEvent triggerEvent;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player" && !collision.isTrigger)
         {
-            spike.ActivateSpike();
             triggerEvent?.Invoke();
         }
     }
